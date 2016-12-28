1e990273becffb9956d3d838a258087fc3116ec3
ginger123 <arielabr.271098@gmail.com>
2016-12-28 12:11:03 +0200

    initial commit
    started Serial manipulation
    added start+stop button

--- 7070SIM/7070SIM/Form1.Designer.cs
+++ 7070SIM/7070SIM/Form1.Designer.cs
@@ -28,20 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(272, 119);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(272, 190);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 1;
+            this.stopButton.Text = "STOP";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 562);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
     }
 }
 
--- 7070SIM/7070SIM/Form1.cs
+++ 7070SIM/7070SIM/Form1.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO.Ports;
+using System.Collections.Concurrent;
 
 namespace _7070SIM
 {
@@ -15,5 +17,78 @@ namespace _7070SIM
         {
             InitializeComponent();
         }
+        public static SerialPort sp;
+        public static ConcurrentQueue<byte[]> msgQueue = new ConcurrentQueue<byte[]>();
+        public static Timer timekeeper = new Timer();
+        public struct settings
+        {
+            public string serialPort;
+            public int baudRate;
+        }
+        public static settings userSetting = new settings();
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            userSetting.serialPort = "COM1";
+            userSetting.baudRate = 9600;
+
+            //
+            sp = new SerialPort(userSetting.serialPort, userSetting.baudRate);
+            sp.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            sp.Open();
+        }
+
+
+        public static void fulltick()
+        {
+
+        }
+        private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            int x = 0;
+            if (sp.ReadChar() != 0xC0) return;
+            string tempmsg = "";
+            while (true)
+            {
+                x = sp.ReadByte();
+                if (x == 0xC0) break;
+                if (x == 0xDB)
+                {
+                    x = sp.ReadByte();
+                    if (x == 0xDC) x = 0xC0;//frame end
+                    if (x == 0xDD) x = 0xDB;//frame escape 
+                }
+                tempmsg += (char)x;
+            }
+            //tempmsg is now without KISS
+            byte[] ret = new byte[tempmsg.Length];
+            for (int i = 0; i < tempmsg.Length; i++)
+            {
+                ret[i] = (byte)tempmsg[i];
+            }
+            msgQueue.Enqueue(ret);
+
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            sp.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timekeeper.Interval = 1000;
+            timekeeper.Tick += new EventHandler(handleCommand);
+        }
+        public static void handleCommand(object sender, EventArgs e)
+        {
+            if (!msgQueue.IsEmpty) return;
+            byte[] currentCommand;
+            msgQueue.TryDequeue(out currentCommand);
+            //first byte is system adress
+            //second byte is command adress
+            //after that it's only params
+        }
+
     }
 }

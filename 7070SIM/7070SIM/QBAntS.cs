c10945a453fe0c85965101de7b660bb551f868dd
adiwwww <USER10@DOMAIN.local>
2017-02-16 19:37:00 +0200

    adding more func
    next:6.2.20 Report antenna 1 deployment activation time

--- 7070SIM/7070SIM/QBAntS.cs
+++ 7070SIM/7070SIM/QBAntS.cs
@@ -11,9 +11,14 @@ namespace _7070SIM
         private bool IsArmed;
         private bool[] IsDeploy = new bool[4];//default is false, already deployed
         private bool[] IsDeploying = new bool[4]; //is currently deploying
+        private bool[] SwitchActivated = new bool[4];
         private byte DeployTime;
         private bool AutoAntdeploying;
-        
+        private byte[] SystemTemp;
+        private bool IsOverride;
+        private bool IndependentBurn;
+        private int[] CountDeploy = new int[4];
+
         public byte[] comm_10101010_reset()//Performs a reset of the microcontroller
         {
             return null;
@@ -32,6 +37,7 @@ namespace _7070SIM
         {
             if (IsArmed && !IsDeploy[0] && AreAntsDeploying() && !AutoAntdeploying)
             {
+                CountDeploy[0]++;
                 DeployStart(0);
             }
             return null;
@@ -40,6 +46,7 @@ namespace _7070SIM
         {
             if (IsArmed && !IsDeploy[1] && AreAntsDeploying() && !AutoAntdeploying)
             {
+                CountDeploy[1]++;
                 DeployStart(1);
             }
             return null;
@@ -48,6 +55,7 @@ namespace _7070SIM
         {
             if (IsArmed && !IsDeploy[2] && AreAntsDeploying() && !AutoAntdeploying)
             {
+                CountDeploy[2]++;
                 DeployStart(2);
             }
             return null;
@@ -56,6 +64,7 @@ namespace _7070SIM
         {
             if (IsArmed && !IsDeploy[3] && AreAntsDeploying() && !AutoAntdeploying)
             {
+                CountDeploy[3]++;
                 DeployStart(3);
             }
             return null;
@@ -63,24 +72,25 @@ namespace _7070SIM
         public void DeployStart(int ant)
         {
             IsDeploying[ant] = true;
+            DeployEnd();
         }
         public void DeployEnd()
         {
             if (DeployTime != 0) throw new Exception("Tried to finish deploying without actually finishing", new InvalidOperationException());
-            
-            int ant=0;
-            for (int i=0;i<4;i++)
+
+            int ant = 0;
+            for (int i = 0; i < 4; i++)
             {
                 if (IsDeploying[i])
                 {
                     IsDeploying[i] = false;
-                    ant=i;
+                    ant = i;
                     break;
                 }
             }
             IsDeploy[ant] = true;
         }
-        private bool AreAntsDeploying()
+        private bool AreAntsDeploying()//return false if deploying
         {
             for (int i = 0; i < 4; i++)
             {
@@ -91,39 +101,38 @@ namespace _7070SIM
         public override void tick()
         {
             if (DeployTime > 0)
-                DeployTime--; 
+                DeployTime--;
             if (DeployTime == 0) DeployEnd();
         }
-        //**this is wait, a func to wait the deploy time before the next deploy
+        /**this is wait, a func to wait the deploy time before the next deploy
         public override void wait()
         {
             byte WaitForDeploy = DeployTime;
             while (WaitForDeploy!=0)
                 WaitForDeploy--;
-        }
+        }*/
+        //deployment switcher = whether the antenna is deployed or not deployed
         public byte[] comm_10100101_AutoAntdeploy()//Start automated sequential antenna deployment
         {
-            
-            if (IsArmed && AreAntsDeploying())
+            if (IsArmed && AreAntsDeploying() && !AutoAntdeploying)
             {
                 AutoAntdeploying = true;
                 comm_10100001_deployAnt1();
-                wait();
+                tick();
                 comm_10100010_deployAnt2();
-                wait();
+                tick();
                 comm_10100011_deployAnt3();
-                wait();
+                tick();
                 comm_10100100_deployAnt4();
+
             }
-            AutoAntdeploying = false;
             return null;
         }
-        //check it!!
-        //deployment switcher = whether the antenna is deployed or not deployed
         public byte[] comm_10111010_deployAnt1_Override()//Deploy antenna 1 with override
         {
             if (IsArmed && AreAntsDeploying() && !AutoAntdeploying)
             {
+                IsOverride = true;
                 DeployStart(0);
             }
             return null;
@@ -132,6 +141,7 @@ namespace _7070SIM
         {
             if (IsArmed && AreAntsDeploying() && !AutoAntdeploying)
             {
+                IsOverride = true;
                 DeployStart(1);
             }
             return null;
@@ -140,6 +150,7 @@ namespace _7070SIM
         {
             if (IsArmed && AreAntsDeploying() && !AutoAntdeploying)
             {
+                IsOverride = true;
                 DeployStart(2);
             }
             return null;
@@ -148,9 +159,48 @@ namespace _7070SIM
         {
             if (IsArmed && AreAntsDeploying() && !AutoAntdeploying)
             {
+                IsOverride = true;
                 DeployStart(3);
             }
             return null;
+        }
+        public byte[] comm_11000000_MeasureAntSysTemp()//Measure the tempeture reported by the tempeture sensor 
+        {
+            return SystemTemp;//check Annex C for convention table
+        }
+        public byte[,] comm_11000011_ReportDeployStatus()
+        {
+            byte[,] FullStatus = new byte[2, 9];
+            FullStatus[0, 0] = Convert.ToByte(SwitchActivated[0]);
+            FullStatus[1, 0] = Convert.ToByte(SwitchActivated[2]);
+
+            FullStatus[0, 1] = Convert.ToByte(IsDeploy[0]);
+            FullStatus[1, 1] = Convert.ToByte(IsDeploy[2]);
+
+            FullStatus[0, 2] = Convert.ToByte(IsDeploying[0]);
+            FullStatus[1, 2] = Convert.ToByte(IsDeploying[2]);
+
+            FullStatus[0, 3] = 0;
+            FullStatus[1, 3] = Convert.ToByte(IndependentBurn);//The antenna system independent burn is currently active.
+
+            FullStatus[0, 4] = Convert.ToByte(SwitchActivated[1]);
+            FullStatus[1, 4] = Convert.ToByte(SwitchActivated[3]);
+
+            FullStatus[0, 5] = Convert.ToByte(IsDeploy[1]);
+            FullStatus[1, 5] = Convert.ToByte(IsDeploy[3]);
+
+            FullStatus[0, 6] = Convert.ToByte(IsDeploying[1]);
+            FullStatus[1, 6] = Convert.ToByte(IsDeploying[3]);
+
+            FullStatus[0, 6] = Convert.ToByte(IsOverride);
+            FullStatus[1, 6] = Convert.ToByte(IsArmed);
+
+
+            return FullStatus;
+        }
+        public int comm_10110000_Ant1DepCount()
+        {
+
         }
 
     }
